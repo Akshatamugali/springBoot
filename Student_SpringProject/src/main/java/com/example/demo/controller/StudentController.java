1b76ed932b74e2b518f84382a7c3330968616fd2
Akshatamugali <akshatamugali9@gmail.com>
2026-02-10 15:18:59 +0530

    Initial Commit.

---
+++ Student_SpringProject/src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,56 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Entity.Student;
+import com.example.demo.service.StudentService;
+
+@RestController
+public class StudentController {
+
+	StudentService studentServ;
+	
+	
+	public StudentController(StudentService studentServ) {
+		super();
+		this.studentServ = studentServ;
+	}
+
+	@PostMapping("/create")
+	public String create(@RequestBody Student st) {
+		studentServ.createStudent(st);
+		return "Student is create";
+	}
+	
+	@GetMapping("/get/{rollNo}")
+	public Student get(@PathVariable int rollNo) {
+		Student st=studentServ.getStudent(rollNo);
+		return st;
+	}
+	
+	@GetMapping("/getall")
+	public List<Student> getAll() {
+		return studentServ.getAllStudent();
+		
+	}
+	
+	@PutMapping("/update")
+	public String update(@RequestBody Student st) {
+		studentServ.updateStudent(st);
+		return "Updated the Student";
+	}
+	
+	@DeleteMapping("/delete/{rollNo}")
+	public String delete(@PathVariable int rollNo) {
+		studentServ.deleteStudent(rollNo);
+		return "Deleted successfully";
+	}
+}
---
+++ Student_SpringProject/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.Entity.Student;
+import com.example.demo.Repository.StudentRepo;
+
+@Service
+public class StudentService {
+	
+	StudentRepo studentrepo;
+	
+
+	public StudentService(StudentRepo studentrepo) {
+		super();
+		this.studentrepo = studentrepo;
+	}
+
+	public void createStudent(Student stu) {
+		studentrepo.save(stu);
+	}
+	
+	public Student getStudent(int rollNo) {
+		Student stu  =studentrepo.findById(rollNo).orElse(null);
+		return stu;
+	}
+	
+	public List<Student> getAllStudent() {
+		List<Student> list = studentrepo.findAll();
+		return list;
+	}
+	public void updateStudent(Student stu) {
+		studentrepo.save(stu);
+	}
+	public void deleteStudent(int rollNo) {
+		studentrepo.deleteById(rollNo);
+	}
+}
